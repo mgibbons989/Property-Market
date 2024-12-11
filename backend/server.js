676931616fd85ab464b80fef2c4a98fe7365b445
James Han <jayimshan@gmail.com>
2024-12-11 17:03:27 -0500

    .emv

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,11 @@ dotenv.config();
 
 const pool = mysql
   .createPool({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
   })
   .promise();
 
