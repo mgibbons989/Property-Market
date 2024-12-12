cced0be9ca0f333a6c19e7249d290221534c3511
madison <mgibbons989@gmail.com>
2024-12-12 18:05:58 -0500

    fixed footer and other styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <p className="footer">
-      &copy; {new Date().getFullYear()} Property Narket James Han, Madison Gibbons, Rayhaan
+    <footer>
+      &copy; {new Date().getFullYear()} Property Market James Han, Madison Gibbons, Rayhaan
       Mohamed
-    </p>
+    </footer>
   );
 }
 
--- src/components/SellerDashboardPage.jsx
+++ src/components/SellerDashboardPage.jsx
@@ -115,7 +115,7 @@ function DashboardPage() {
     if (newCardData.photo) {
       formData.append("photo", newCardData.photo);
     }
-  
+
     try {
       if (editingCard) {
         // Edit property
@@ -199,7 +199,7 @@ function DashboardPage() {
           <div className="grid">
             {/* Button card to add new information */}
             <div className="card add-card" onClick={toggleModal}>
-              <p>Add Property</p>
+              <h2>Add Property</h2>
               <img src={plus} alt="plus icon" />
             </div>
 
@@ -231,15 +231,15 @@ function DashboardPage() {
                     />
                   )}
                   <div className="card-details">
-                    <p>
+                    <li>
                       <strong>Location:</strong> {card.location}
-                    </p>
-                    <p>
+                    </li>
+                    <li>
                       <strong>Bedrooms:</strong> {card.bedrooms}
-                    </p>
-                    <p>
-                      <strong>Age:</strong> {card.age}
-                    </p>
+                    </li>
+                    <li>
+                      <strong>Age:</strong> {card.age} year(s) old
+                    </li>
                   </div>
                 </div>
               ))
@@ -452,19 +452,21 @@ function DashboardPage() {
 
               {/* Photo Upload */}
               <div className="image-col">
-                <div className="card">
+                <div className="card addImage">
                   <label htmlFor="photo">Add Photo</label>
-                  <input
-                    type="file"
-                    id="photo"
-                    accept="image/*"
-                    onChange={(e) =>
-                      setNewCardData({
-                        ...newCardData,
-                        photo: e.target.files[0],
-                      })
-                    }
-                  />
+                  <div className="imgInput">
+                    <input
+                      type="file"
+                      id="photo"
+                      accept="image/*"
+                      onChange={(e) =>
+                        setNewCardData({
+                          ...newCardData,
+                          photo: e.target.files[0],
+                        })
+                      }
+                    />
+                  </div>
                 </div>
               </div>
             </div>
