5df5aa7f282a1514bc2e26035ef43498f38b234a
James Han <jayimshan@gmail.com>
2024-12-12 18:05:29 -0500

    edit property photo url issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <p className="footer">
-      &copy; {new Date().getFullYear()} Property Narket James Han, Madison Gibbons, Rayhaan
-      Mohamed
+      &copy; {new Date().getFullYear()} Property Narket James Han, Madison
+      Gibbons, Rayhaan Mohamed
     </p>
   );
 }
